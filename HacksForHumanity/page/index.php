92bed62a2720b0c5a66fa0cdaae6ab9aabecde53
Benjamin <blladi610@yahoo.com>
2016-10-08 20:51:42 -0700

    Recreated the folder and files to remove spaces in the names

---
+++ HacksForHumanity/page/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+	<h1>Ben Is Awesome!</h1>
+</body>
+</html>
