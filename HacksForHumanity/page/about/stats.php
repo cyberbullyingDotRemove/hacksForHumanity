1d068191ddd27046ee9a9adb68b66bd69bb3a77c
Tyman Sin <sin.tyman@yahoo.com>
2016-10-09 11:20:36 -0700

    created stats.php

---
+++ HacksForHumanity/page/about/stats.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>cyber_go(ne) -- Our Mission</title>
+<?php
+include_once ("../link.php");
+?>
+</head>
+<body>
+	<div class="wrapper">
+<?php
+include_once ("../header.php");
+?>
+	<div class="body">
+			<div>
+				
+			</div>
+		</div>
+<?php
+include_once ("../footer.php");
+?>
+</div>
+</body>
+
+</html>
