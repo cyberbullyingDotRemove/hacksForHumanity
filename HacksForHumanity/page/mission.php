2f020d2512fe4c71f85833abe17544c371b69c0f
Tyman Sin <sin.tyman@yahoo.com>
2016-10-09 09:25:31 -0700

    Created mission.php

---
+++ HacksForHumanity/page/mission.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>cyber_go(ne) -- Our Mission</title>
+<?php
+	include_once("link.php");
+?>
+</head>
+<body>
+<?php
+	include_once("header.php");
+?>
+	<div>
+		<h2>Our Mission</h2>
+		<div>
+			<p>
+				cyber_go(ne) is a team of coders and developers
+				who are dedicated to bringing an end to cyberbullying.
+				By providing cyber-citizens the ability to anonymously
+				report instances of cyberbullying and by implementing
+				a reward system to encourage them to actively continue
+				reporting, cyber_go(ne) hopes to make cyberbullying a
+				relavent issue that people actively work together to
+				stop.
+			</p>
+		</div>
+	</div>
+<?php 
+	include_once("footer.php");
+?>
+</body>
+
+</html>
